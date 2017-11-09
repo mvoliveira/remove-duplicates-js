cbaf7c513828a99604f7bb236a2e1707b4288ec1
Matheus <matheus.mvo10@gmail.com>
2017-11-09 21:28:36 -0200

    add first exercise

---
+++ atv1.js
@@ -0,0 +1,44 @@
+// array_zika = [1,2,3,4]
+array_zika = [1,1,1,1]
+// array_zika = [1,1,2,2]
+function compara(array){
+  //   for (var i = 0; i < array.length-1; i++) {
+  //     console.log(i);
+  //     // temp_index = 0
+  //     temp_index= 1 + i
+  //     console.log(temp_index);
+  //     if (array[i] === array[temp_index]){
+  //       array.splice(array, temp_index, 1)
+  //       console.log(array);
+  //       console.log("iguais");
+  //     }
+  //     else {
+  //       console.log("diferentes");
+  //       console.log(array_zika);
+  //     }
+  //   }
+  // }
+  //
+
+
+}
+// compara(array_zika)
+
+
+
+
+
+
+
+
+function imprime_index(element, index, array) {
+  console.log(element);
+console.log(index+1);
+  if (array.indexOf(element) == array[index+1]){
+  //  console.log(array.indexOf(index));
+    console.log("achou igual");
+  }
+
+}
+
+array_zika.forEach(imprime_index)
