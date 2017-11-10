a3434cf43fb561900f22d9ec2f737f2d0b0ac87e
Matheus <matheus.mvo10@gmail.com>
2017-11-09 22:29:24 -0200

    refactoring

--- atv1.js
+++ atv1.js
@@ -1,44 +1,19 @@
-// array_test = [1,2,3,4]
-array_test = [1,1,1,1]
-// array_test = [1,1,2,2]
-function compara(array){
-  //   for (var i = 0; i < array.length-1; i++) {
-  //     console.log(i);
-  //     // temp_index = 0
-  //     temp_index= 1 + i
-  //     console.log(temp_index);
-  //     if (array[i] === array[temp_index]){
-  //       array.splice(array, temp_index, 1)
-  //       console.log(array);
-  //       console.log("iguais");
-  //     }
-  //     else {
-  //       console.log("diferentes");
-  //       console.log(array_test);
-  //     }
-  //   }
-  // }
-  //
-
-
-}
-// compara(array_test)
-
-
-
-
-
-
-
-
-function imprime_index(element, index, array) {
-  console.log(element);
-console.log(index+1);
-  if (array.indexOf(element) == array[index+1]){
-  //  console.log(array.indexOf(index));
-    console.log("achou igual");
+function remove_duplicates(array){
+  for (var i = 0; i < array.length; i++) {
+    increment= i + 1
+    if (array[i]===array[increment]) {
+      console.log("removeu");
+      array.splice(array[i])
+    } else {
+      increment
+      console.log("nao removeu");
+    }
   }
-
+  console.log(array);
 }
 
-array_test.forEach(imprime_index)
+
+
+// remove_duplicates([1,2,3,4])
+// remove_duplicates([1,1,1,1])
+remove_duplicates([1,1,2,2])
