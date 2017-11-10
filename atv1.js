23a8e96b9c81565d05b283d048654a3da670cfdf
Matheus <matheus.mvo10@gmail.com>
2017-11-09 22:37:36 -0200

    working and refactoring

--- atv1.js
+++ atv1.js
@@ -2,18 +2,17 @@ function remove_duplicates(array){
   for (var i = 0; i < array.length; i++) {
     increment= i + 1
     if (array[i]===array[increment]) {
-      console.log("removeu");
-      array.splice(array[i])
+      array.splice(array[increment],1)
+
     } else {
-      increment
-      console.log("nao removeu");
+      increment+1
     }
   }
-  console.log(array);
+console.log(array);
 }
 
 
 
-// remove_duplicates([1,2,3,4])
-// remove_duplicates([1,1,1,1])
+remove_duplicates([1,2,3,4])
+remove_duplicates([1,1,1,1])
 remove_duplicates([1,1,2,2])
